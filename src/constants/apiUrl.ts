0a34a72f159a5d8ef5d030ecc631e2b3936d9445
BingTianEng <92073136+BingTianEng@users.noreply.github.com>
2022-04-18 09:28:09 +1000

    Update apiUrl.ts

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,3 +1,3 @@
 export const DEVELOPMENT_API_URL = 'http://localhost:5000';
-export const UAT_API_URL = 'http://alb-220409-1878828322.us-east-1.elb.amazonaws.com';
-export const PRODUCTION_API_URL = 'http://alb-220409-1878828322.us-east-1.elb.amazonaws.com';
+export const UAT_API_URL = 'https://service.uat.ecodev.link';
+export const PRODUCTION_API_URL = 'https://service.uat.ecodev.link';
